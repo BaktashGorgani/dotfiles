1e86472154936da58902ec2502dd48168c333a99
Baktash Gorgani <baktash.gorgani@gmail.com>
2025-05-11 16:22:41 -0400

    chore(zsh): fix comment and add chezmoi completion

--- dot_oh-my-zsh/custom/miscellaneous.zsh
+++ dot_oh-my-zsh/custom/miscellaneous.zsh
@@ -4,3 +4,6 @@ eval "$(gh copilot alias -- zsh)"
 # Oh My Posh
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/night-owl.omp.json)"
+
+# chezmoi completion
+eval "$(chezmoi completion zsh)"
