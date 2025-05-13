1f7b8f519e0bd2dfaff07a202860bdd3afd139bc
Baktash Gorgani <baktash.gorgani@gmail.com>
2025-05-13 01:37:54 -0400

    feat(env): add template for environment variables
    Testing templates. They are quite intuitive and easy to write.
    This template is tested and works as expected.

--- dot_zsh_custom/environment.zsh
+++
@@ -1,7 +0,0 @@
-export EDITOR=nvim
-export VISUAL=nvim
-export SUDO_EDITOR=nvim
-export BAT_THEME="OneHalfDark"
-export XCURSOR_THEME="material_cursors"
-export XCURSOR_SIZE="32"
-export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:/home/baky/.local/share/flatpak/exports/share:$XDG_DATA_DIRS"
